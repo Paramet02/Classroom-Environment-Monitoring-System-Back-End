8607e0f9c41d8b9c936c5598541f38946bf17c9b
SiravitB <bellsiravit@gmail.com>
2025-04-21 17:20:28 +0700

    update

--- authMiddleware.js
+++ authMiddleware.js
@@ -1,16 +1,16 @@
 const jwt = require("jsonwebtoken");
 
-const verifyToken = (req, res, next) => {
-  const token = req.headers["authorization"];
+function getUserFromToken(req, res, next) {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
 
-  if (!token) return res.status(403).json({ error: "No token provided" });
-
-  jwt.verify(token.split(" ")[1], process.env.JWT_SECRET, (err, decoded) => {
-    if (err) return res.status(401).json({ error: "Unauthorized" });
-    
-    req.user = decoded; 
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
     next();
-  });
-};
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+}
 
-module.exports = verifyToken;
+module.exports = getUserFromToken;
--- server.js
+++ server.js
@@ -14,11 +14,9 @@ app.use(registerRoute);
 app.use(loginRoute);
 app.use(profileRoute);
 
-// ✅ เพิ่ม route สำหรับ Air Quality โดยตรง
-// ✅ ใช้ router air-quality
+
 app.use("/air-quality", airQualityRoute);
 
-// ⛔️ ❌ ห้ามมีพวก app.post("/predict-air-quality", ...) หรือ app.post("/health-check", ...) ซ้ำซ้อนอีก
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
